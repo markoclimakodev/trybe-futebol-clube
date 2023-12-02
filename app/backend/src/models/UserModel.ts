feeb8294bdebd31e0e124a368ea49ca74f6b3333
markoclimako <mkclimako@gmail.com>
2023-12-02 16:09:40 -0300

    feat: implement findRole in UserModel

--- app/backend/src/models/UserModel.ts
+++ app/backend/src/models/UserModel.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcryptjs';
 import ILogin from '../Interfaces/User/ILogin';
+import { IRole } from '../Interfaces/User/IUser';
 import IUserModel from '../Interfaces/User/IUserModel';
 import SequelizeUser from '../database/models/SequelizeUser';
 import tokenGenerate from '../utils/tokenGenerate';
@@ -13,11 +14,18 @@ export default class UserModel implements IUserModel {
 
     if (user && bcrypt.compareSync(password, user.password)) {
       return tokenGenerate({
-        username: user.username,
-        email: user.email,
+        email, password,
       });
     }
 
+    return null;
+  }
+
+  async findRole(login: ILogin): Promise<IRole | null> {
+    const user = await this.model.findOne({ where: { email: login.email } });
+    if (user) {
+      return { role: user.role };
+    }
     return null;
   }
 }
