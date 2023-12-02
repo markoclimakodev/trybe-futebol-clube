c977a1f13ac970d250f2527f58ca6e2b57b2c42e
markoclimako <mkclimako@gmail.com>
2023-12-02 16:10:11 -0300

    feat: add findRole in IUserModel interface

--- app/backend/src/Interfaces/User/IUserModel.ts
+++ app/backend/src/Interfaces/User/IUserModel.ts
@@ -1,5 +1,7 @@
 import ILogin from './ILogin';
+import { IRole } from './IUser';
 
 export default interface IUserModel {
   login(login:ILogin): Promise<string | null>
+  findRole(login:ILogin): Promise<IRole | null>
 }
