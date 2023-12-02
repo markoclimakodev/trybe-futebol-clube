8404cc1d5e78d066e99d4304f7f301572c963bca
markoclimako <mkclimako@gmail.com>
2023-12-02 16:13:13 -0300

    feat: implement 'login/role' in user routes

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,10 +1,16 @@
 import { Request, Response, Router } from 'express';
 import UserController from '../controllers/UserController';
 import validateLogin from '../middlewares/loginMiddleware';
+import auth from '../middlewares/authMiddleware';
 
 const userController = new UserController();
 const router = Router();
 
 router.post('/', validateLogin, (req:Request, res:Response) => userController.login(req, res));
+router.post(
+  '/role',
+  auth,
+  (req:Request, res:Response) => userController.findRole(req, res),
+);
 
 export default router;
