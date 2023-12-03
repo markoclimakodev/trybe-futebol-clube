babe2325b140110cd9f7abe328f8a97ab8c5bdad
markoclimako <mkclimako@gmail.com>
2023-12-03 19:51:17 -0300

    refactor: refactor findRole method in UserController

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -16,10 +16,9 @@ export default class UserController {
     res.status(200).json({ token });
   }
 
-  public async findRole(req: Request, res: Response) {
-    const login = req.body;
-    const role = await this.userSerive.findRole(login);
+  static async findRole(req: Request, res: Response) {
+    const { role } = req.body;
 
-    res.status(200).json(role);
+    res.status(200).json({ role });
   }
 }
