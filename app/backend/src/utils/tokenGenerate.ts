a69c2cd314d3c3cb21174207fc00ef1cf75e2d60
markoclimako <mkclimako@gmail.com>
2023-12-03 19:41:43 -0300

    refactor: remove tokenGenerate

--- app/backend/src/utils/tokenGenerate.ts
+++
@@ -1,8 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-import TokenPayload from '../Interfaces/User/TokenPayload';
-
-export default function tokenGenerate(payLoad:TokenPayload):string {
-  const secret = process.env.JWT_SECRET ?? 'jwt_secret';
-  const token = jwt.sign(payLoad, secret, { expiresIn: '8h' });
-  return token;
-}
