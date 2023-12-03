3f2a3974de3b4a9dc50ff27100a6f947118908c9
markoclimako <mkclimako@gmail.com>
2023-12-02 22:40:23 -0300

    refactor: refactor auth and validateLogin middlewares

--- app/backend/src/middlewares/authMiddleware.ts
+++ app/backend/src/middlewares/authMiddleware.ts
@@ -1,19 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import UnauthorizedError from '../helpers/ApiError/UnauthorizedError';
 
 export default async function auth(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
 
-  if (!authorization) {
-    return res.status(401).json({ message: 'Token not found' });
-  }
+  if (!authorization) throw new UnauthorizedError('Token not found');
+  const token = authorization.split(' ')[1];
 
-  try {
-    const secret = process.env.JWT_SECRET || 'jwt_secret';
-    const user = jwt.verify(authorization, secret);
-    req.body.user = user;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Token must be a valid token' });
-  }
+  const secret = process.env.JWT_SECRET || 'jwt_secret';
+  const user = jwt.verify(token, secret);
+
+  if (!user) throw new UnauthorizedError('Token must be a valid token');
+
+  next();
 }
--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import BadRequestError from '../helpers/ApiError/BadRequestError';
+import UnauthorizedError from '../helpers/ApiError/UnauthorizedError';
 import { loginFieldsValiation, loginValuesValidation } from '../utils/validation/loginValidation';
 
 export default function validateLogin(req:Request, res:Response, next:NextFunction) {
   const loginError = loginFieldsValiation(req.body);
-  if (loginError) return res.status(400).json({ message: loginError });
+  if (loginError) throw new BadRequestError('All fields must be filled');
 
   const valuesError = loginValuesValidation(req.body);
-  if (valuesError) return res.status(401).json({ message: valuesError });
+  if (valuesError) throw new UnauthorizedError('Invalid email or password');
 
   next();
 }
