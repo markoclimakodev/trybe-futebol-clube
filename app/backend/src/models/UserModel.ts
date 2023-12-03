36973f5d65152ed2c992e875f7560cf73ec43d11
markoclimako <mkclimako@gmail.com>
2023-12-03 19:52:14 -0300

    refactor: remove findRole method from UserModel

--- app/backend/src/models/UserModel.ts
+++ app/backend/src/models/UserModel.ts
@@ -1,9 +1,8 @@
 import * as bcrypt from 'bcryptjs';
 import ILogin from '../Interfaces/User/ILogin';
-import { IRole } from '../Interfaces/User/IUser';
 import IUserModel from '../Interfaces/User/IUserModel';
 import SequelizeUser from '../database/models/SequelizeUser';
-import tokenGenerate from '../utils/tokenGenerate';
+import JWTTokenGenerator from '../utils/JWTTokenGenerator';
 
 export default class UserModel implements IUserModel {
   private model = SequelizeUser;
@@ -12,17 +11,13 @@ export default class UserModel implements IUserModel {
     const user = await this.model.findOne({ where: { email: login.email } });
 
     if (user && bcrypt.compareSync(login.password, user.password)) {
-      return tokenGenerate({ id: user.id });
+      return JWTTokenGenerator.generateToken({
+        id: user.id,
+        role: user.role,
+        email: user.email,
+      });
     }
 
-    return null;
-  }
-
-  async findRole(login: ILogin): Promise<IRole | null> {
-    const user = await this.model.findOne({ where: { email: login.email } });
-    if (user) {
-      return { role: user.role };
-    }
     return null;
   }
 }
