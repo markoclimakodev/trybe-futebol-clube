b92a6112db2b7b3a63bcda42b5db295bd3fc89b5
markoclimako <mkclimako@gmail.com>
2023-12-03 19:52:00 -0300

    refactor: remove findRole method from UserService

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,5 +1,4 @@
 import ILogin from '../Interfaces/User/ILogin';
-import { IRole } from '../Interfaces/User/IUser';
 import IUserModel from '../Interfaces/User/IUserModel';
 import UserModel from '../models/UserModel';
 
@@ -8,16 +7,6 @@ export default class UserService {
 
   public async login(login:ILogin):Promise<string | null> {
     const token = await this.userModel.login(login);
-    if (!token) return null;
-
     return token;
-  }
-
-  public async findRole(login:ILogin):Promise<IRole | null> {
-    const role = await this.userModel.findRole(login);
-    if (!role) {
-      return null;
-    }
-    return role;
   }
 }
