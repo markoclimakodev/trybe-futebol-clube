1abb33be4ad0af3b31c084bc5845a0a759e5fb8a
markoclimako <mkclimako@gmail.com>
2023-12-03 19:41:24 -0300

    refactor: remove hashGenerate

--- app/backend/src/utils/hashGenerate.ts
+++
@@ -1,6 +0,0 @@
-import * as bcrypt from 'bcryptjs';
-
-export default function hashGenerate(password:string):string {
-  const salt = bcrypt.genSaltSync(10);
-  return bcrypt.hashSync(password, salt);
-}
