935e9178280408f9e4bef1477e4d7fb4ec6f5909
markoclimako <mkclimako@gmail.com>
2023-12-03 19:47:20 -0300

    refactor: remove findRole from IUserModel

--- app/backend/src/Interfaces/User/IUserModel.ts
+++ app/backend/src/Interfaces/User/IUserModel.ts
@@ -1,7 +1,5 @@
 import ILogin from './ILogin';
-import { IRole } from './IUser';
 
 export default interface IUserModel {
   login(login:ILogin): Promise<string | null>
-  findRole(login:ILogin): Promise<IRole | null>
 }
