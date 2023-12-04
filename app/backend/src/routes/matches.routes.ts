1c3c2987271f8f9e43a8129fbce0602575f959dc
markoclimako <mkclimako@gmail.com>
2023-12-04 03:06:44 -0300

    feat: add patch matches/:id/finish endpoint to  routes

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,10 +1,17 @@
 import { Request, Response, Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
+import Auth from '../middlewares/Auth';
 
 const matchesController = new MatchesController();
 
 const router = Router();
 
 router.get('/', (req:Request, res:Response) => matchesController.getMatches(req, res));
+router.patch(
+  '/:id/finish',
+  Auth.validate,
+
+  (req:Request, res:Response) => matchesController.finishMatch(req, res),
+);
 
 export default router;
