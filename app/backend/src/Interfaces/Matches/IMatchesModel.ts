95486ebbeb31332a8f9c45b2fae31498221bb165
markoclimako <mkclimako@gmail.com>
2023-12-04 06:43:51 -0300

    feat: add updateMatch, verifyTeamsExist and createMatch in IMatchesModel interface

--- app/backend/src/Interfaces/Matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/Matches/IMatchesModel.ts
@@ -1,8 +1,11 @@
+import IMatchCreate from './IMatchCreate';
 import IMatches from './IMatches';
 
 export default interface IMatchesModel {
   getMatches():Promise<IMatches[]>
   getMatchesByProgress(progress: boolean):Promise<IMatches[]>
   finishMatch(id:number):Promise<[number]>
+  updateMatch(id: number, match: IMatches): Promise<[number]>
+  verifyTeamsExist(homeTeamId: number, awayTeamId: number): Promise<boolean>
+  createMatch(newMatch:IMatchCreate):Promise<IMatches>
 }
-/** https://cursos.alura.com.br/forum/topico-error-ts2322-type-affectedcount-number-is-not-assignable-to-type-number-produto-source-has-1-element-s-but-target-requires-2-223505  */
