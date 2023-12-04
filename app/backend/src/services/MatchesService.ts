c1397117a21d57968a85acaaf9846728ea1b0074
markoclimako <mkclimako@gmail.com>
2023-12-04 06:41:50 -0300

    feat: add updateMatch and createMatch in MatchesService class

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,3 +1,4 @@
+import IMatchCreate from '../Interfaces/Matches/IMatchCreate';
 import IMatches from '../Interfaces/Matches/IMatches';
 import IMatchesModel from '../Interfaces/Matches/IMatchesModel';
 import MatchesModel from '../models/MatchesModel';
@@ -7,12 +8,12 @@ export default class MatchesService {
     private matchesModel: IMatchesModel = new MatchesModel(),
   ) {}
 
-  public async getMatches(progress: string | undefined): Promise<IMatches[]> {
-    if (progress !== undefined) {
-      const inProgress = progress === 'true';
-      const filteredMatches = await this.matchesModel.getMatchesByProgress(inProgress);
+  public async getMatches(matchInProgress: string | undefined): Promise<IMatches[]> {
+    if (matchInProgress !== undefined) {
+      const isInProgress = matchInProgress === 'true';
+      const filteredMatchesByProgress = await this.matchesModel.getMatchesByProgress(isInProgress);
 
-      return filteredMatches;
+      return filteredMatchesByProgress;
     }
 
     const allMatches = await this.matchesModel.getMatches();
@@ -21,7 +22,16 @@ export default class MatchesService {
   }
 
   public async finishMatch(id:number) {
-    const match = await this.matchesModel.finishMatch(id);
-    return match;
+    const updatedMatch = await this.matchesModel.finishMatch(id);
+    return updatedMatch;
+  }
+
+  public async updateMatch(id: number, match: IMatches): Promise<void> {
+    await this.matchesModel.updateMatch(id, match);
+  }
+
+  public async createMatch(newMatch:IMatchCreate):Promise<IMatches> {
+    const createdMatch = await this.matchesModel.createMatch(newMatch);
+    return createdMatch;
   }
 }
