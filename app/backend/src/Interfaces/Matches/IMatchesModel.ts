f8a70dd6cc341cb23f5f105f8db9c704d445bcb1
markoclimako <mkclimako@gmail.com>
2023-12-04 01:33:15 -0300

    feat: add getMatchesByProgress in IMatchesModel interface

--- app/backend/src/Interfaces/Matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/Matches/IMatchesModel.ts
@@ -1,5 +1,6 @@
 import IMatches from './IMatches';
 
 export default interface IMatchesModel {
-  getAllMatches():Promise<IMatches[]>
+  getMatches():Promise<IMatches[]>
+  getMatchesByProgress(progress: boolean):Promise<IMatches[]>
 }
