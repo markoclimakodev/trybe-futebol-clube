358b464613d0f433b1c9f563b2e7b35d20ff083a
markoclimako <mkclimako@gmail.com>
2023-12-04 06:44:45 -0300

    feat: add post  endpoint to  routes

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
 import Auth from '../middlewares/Auth';
+import Match from '../middlewares/Match';
+import AuthMatch from '../middlewares/AuthMatch';
 
 const matchesController = new MatchesController();
 
@@ -8,10 +10,20 @@ const router = Router();
 
 router.get('/', (req:Request, res:Response) => matchesController.getMatches(req, res));
 router.patch(
+  '/:id',
+  AuthMatch.validate,
+  (req: Request, res: Response) => matchesController.updateMatch(req, res),
+);
+router.patch(
   '/:id/finish',
   Auth.validate,
-
   (req:Request, res:Response) => matchesController.finishMatch(req, res),
+);
+router.post(
+  '/',
+  AuthMatch.validate,
+  Match.validate,
+  (req:Request, res:Response) => matchesController.createMatch(req, res),
 );
 
 export default router;
