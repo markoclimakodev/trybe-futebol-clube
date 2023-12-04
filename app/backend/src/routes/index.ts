8ecc27d8b65f14b10312d79ae75ba4b6d558a71e
markoclimako <mkclimako@gmail.com>
2023-12-03 21:54:08 -0300

    feat: implement matches routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import matchesRouter from './matches.routes';
 import teamRouter from './team.routes';
 import userRouter from './user.routes';
 
@@ -6,5 +7,6 @@ const router = Router();
 
 router.use('/teams', teamRouter);
 router.use('/login', userRouter);
+router.use('/matches', matchesRouter);
 
 export default router;
