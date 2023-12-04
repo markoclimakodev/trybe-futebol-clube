44ada65c1635e78679ebcffc44edc68476227fec
markoclimako <mkclimako@gmail.com>
2023-12-04 03:03:31 -0300

    feat: add finishMatch in IMatchesModel interface

--- app/backend/src/Interfaces/Matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/Matches/IMatchesModel.ts
@@ -3,4 +3,6 @@ import IMatches from './IMatches';
 export default interface IMatchesModel {
   getMatches():Promise<IMatches[]>
   getMatchesByProgress(progress: boolean):Promise<IMatches[]>
+  finishMatch(id:number):Promise<[number]>
 }
+/** https://cursos.alura.com.br/forum/topico-error-ts2322-type-affectedcount-number-is-not-assignable-to-type-number-produto-source-has-1-element-s-but-target-requires-2-223505  */
