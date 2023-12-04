f7709ce6ca631501fbee5a1952eceb7d9cd0113d
markoclimako <mkclimako@gmail.com>
2023-12-04 01:35:26 -0300

    feat: add getMatchesByProgress in MatchesModel class

--- app/backend/src/models/MatchesModel.ts
+++ app/backend/src/models/MatchesModel.ts
@@ -5,21 +5,31 @@ import SequelizeTeam from '../database/models/SequelizeTeam';
 
 export default class MatchesModel implements IMatchesModel {
   private model = SequelizeMatches;
+  teamModel = [
+    {
+      model: SequelizeTeam,
+      as: 'homeTeam',
+      attributes: ['teamName'],
+    },
+    {
+      model: SequelizeTeam,
+      as: 'awayTeam',
+      attributes: ['teamName'],
+    },
+  ];
 
-  async getAllMatches(): Promise<IMatches[]> {
-    const matches = await this.model.findAll({ include: [
-      {
-        model: SequelizeTeam,
-        as: 'homeTeam',
-        attributes: ['teamName'],
-      },
-      {
-        model: SequelizeTeam,
-        as: 'awayTeam',
-        attributes: ['teamName'],
-      },
-    ] });
+  async getMatches(): Promise<IMatches[]> {
+    const matches = await this.model.findAll({ include: this.teamModel });
 
     return matches;
+  }
+
+  async getMatchesByProgress(progress:boolean):Promise<IMatches[]> {
+    const filteredMatches = await this.model.findAll({
+      where: { inProgress: progress },
+      include: this.teamModel,
+    });
+
+    return filteredMatches;
   }
 }
