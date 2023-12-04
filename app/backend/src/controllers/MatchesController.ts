fb4d04e027eb4492a947692965d6c44e2944e33e
markoclimako <mkclimako@gmail.com>
2023-12-04 01:37:56 -0300

    refactor: update getMatches method in MatchesController to handle filtering of matches based on progress

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -4,8 +4,10 @@ import MatchesService from '../services/MatchesService';
 export default class MatchesController {
   constructor(private matchesService = new MatchesService()) {}
 
-  public async getAllMatches(_req:Request, res:Response) {
-    const matches = await this.matchesService.getAllMatches();
+  public async getMatches(req:Request, res:Response) {
+    const { inProgress } = req.query;
+    const progress = typeof inProgress === 'string' ? inProgress : undefined;
+    const matches = await this.matchesService.getMatches(progress);
     res.status(200).json(matches);
   }
 }
