750d1e6c8818919cd533864b4f614fd42863da11
markoclimako <mkclimako@gmail.com>
2023-12-05 03:22:47 -0300

    feat: implement leaderboard routes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import leaderBoardRouter from './leaderBoard.routes';
 import matchesRouter from './matches.routes';
 import teamRouter from './team.routes';
 import userRouter from './user.routes';
@@ -8,5 +9,6 @@ const router = Router();
 router.use('/teams', teamRouter);
 router.use('/login', userRouter);
 router.use('/matches', matchesRouter);
+router.use('/leaderboard', leaderBoardRouter);
 
 export default router;
